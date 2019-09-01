6e8829bfe5ff84a77114e6e8312030f3afd7f566
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-01 19:09:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,11 +9,11 @@ def turn_count(board)
   counter 
 end 
 
-def current_player(board) 
-  if turn_count(board) % 2 == 0 
-    puts "X"
-  else 
-    puts "O"
-  end   
-end   
+# def current_player(board) 
+#   if turn_count(board) % 2 == 0 
+#     puts "X"
+#   else 
+#     puts "O"
+#   end   
+# end   
 
