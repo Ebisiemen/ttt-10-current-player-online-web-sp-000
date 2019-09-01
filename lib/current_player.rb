42e4f298805e95215b997208b8497efd90f62cde
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-01 17:58:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -11,14 +11,3 @@ end
 
 # ["x", "", "o"]
 
-# def turn_count(board)
-#   counter = 0 
-#   turns = 0 
-#   while counter < 9 
-#     if board(index) == " "
-#     counter += 1 
-#     else turns += 1 && counter += 1  
-#     end 
-#   end   
-# end   
-  
