692430aa3b89e6ae4e3669e73d49dfd6870f8666
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-09-01 18:29:20 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,9 +3,9 @@ def turn_count(board)
   counter = 0
   board.each { |element| 
     if element == "X" || "O" 
-     counter = counter + 1 
+     counter = counter += 1 
     else 
-     counter = counter + 0 
+     counter = counter += 0 
     end }
 end 
 
